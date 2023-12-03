1a29f93f26a54f39d08b576ccfe228cdcad0da8a
Edward Tam <edward.tam@flipp.com>
2023-12-03 15:53:20 -0500

    README

--- mappings.lua
+++ mappings.lua
@@ -4,18 +4,34 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- Find files only in version control. Very helpful for filtering huge folders like node_modules.
     ["<leader>fg"] = { "<cmd> Telescope git_files <cr>", "Find git files" },
-    ["<leader>lz"] = { "<cmd> Lazy <cr>", "Open Lazy Panel" },
-    ["<leader>ls"] = { "<cmd> Mason <cr>", "Open Mason Panel" },
 
-    ["<leader>db"] = { "<cmd> lua require('dap').toggle_breakpoint() <cr>", "Toggle Debugger Breakpoint" },
-    ["<leader>dd"] = { "<cmd> lua require('dapui').open() <cr>", "Open Debugger Session" },
-    ["<leader>dx"] = { "<cmd> lua require('dapui').close() <cr>", "Close Debugger Session" },
-    ["<leader>dc"] = { "<cmd> lua require('dap').continue() <cr>", "Continue Debugger Session" },
-    ["<leader>df"] = { "<cmd> lua require('dap').clear_breakpoints() <cr>", "Clear all Breakpoints" },
-
+    -- Global search by text
     ["<leader>lg"] = { "<cmd> Telescope live_grep search_dirs=. <cr>", "Find in files" },
 
+    -- Shorthand to open Lazy panel
+    ["<leader>lz"] = { "<cmd> Lazy <cr>", "Open Lazy Panel" },
+
+    -- Shorthand to open Mason panel
+    ["<leader>ls"] = { "<cmd> Mason <cr>", "Open Mason Panel" },
+
+    -- add a breakpoint to current cursor location
+    ["<leader>db"] = { "<cmd> lua require('dap').toggle_breakpoint() <cr>", "Toggle Debugger Breakpoint" },
+
+    -- open the debugger UI
+    ["<leader>dd"] = { "<cmd> lua require('dapui').open() <cr>", "Open Debugger Session" },
+
+    -- close the debugger UI
+    ["<leader>dx"] = { "<cmd> lua require('dapui').close() <cr>", "Close Debugger Session" },
+
+    -- start a debugging session
+    ["<leader>dc"] = { "<cmd> lua require('dap').continue() <cr>", "Continue Debugger Session" },
+
+    -- clear all breakpoints
+    ["<leader>df"] = { "<cmd> lua require('dap').clear_breakpoints() <cr>", "Clear all Breakpoints" },
+
+    -- open the LazyGit panel
     ["<leader>gg"] = { "<cmd> LazyGit <cr>", "Open LazyGit" },
   },
   v = {
